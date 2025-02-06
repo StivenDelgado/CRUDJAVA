d337537d3a82043ed12dc0232fe9c9799dab6974
StivenDelgado <delgadostiven123@gmail.com>
2025-02-05 23:01:56 -0500

    First commit maven project created

---
+++ src/main/java/CRUDJAVA/CRUDJAVA/App.java
@@ -0,0 +1,13 @@
+package CRUDJAVA.CRUDJAVA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
